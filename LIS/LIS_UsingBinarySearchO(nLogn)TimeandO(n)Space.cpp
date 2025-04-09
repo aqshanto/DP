36dc9d5bc4e3767b4bf539920eb8166aef3cad98
aqShanto <abdulquymshanto@gmail.com>
2025-04-09 16:59:52 +0600

    up

--- LIS/LIS_UsingBinarySearchO(nLogn)TimeandO(n)Space.cpp
+++ LIS/LIS_UsingBinarySearchO(nLogn)TimeandO(n)Space.cpp
@@ -1,50 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> temp;
+int longestIncreasingSubsequence(vector<int>& arr, int n) {
+    // Create a temporary vector to store the increasing subsequence
+    temp.push_back(arr[0]);
 
-vector<int> ans;
-int lengthOfLIS(vector<int>& v) {
-    int n = v.size();
-    // Initialize the answer vector with the
-    // first element of v
-    ans.push_back(v[0]);
+    int len = 1;
+
     for (int i = 1; i < n; i++) {
-        if (v[i] > ans.back()) {
-            // If the current number is greater
-            // than the last element of the answer
-            // vector, it means we have found a
-            // longer increasing subsequence.
-            // Hence, we append the current number
-            // to the answer vector.
-            ans.push_back(v[i]);
+        if (arr[i] > temp.back()) {
+            // If arr[i] is greater than the last element of temp, extend the subsequence
+            temp.push_back(arr[i]);
+            len++;
         } else {
-            // If the current number is not
-            // greater than the last element of
-            // the answer vector, we perform
-            // a binary search to find the smallest
-            // element in the answer vector that
-            // is greater than or equal to the
-            // current number.
-
-            // The lower_bound function returns
-            // an iterator pointing to the first
-            // element that is not less than
-            // the current number.
-            int low = lower_bound(ans.begin(), ans.end(),
-                                  v[i]) -
-                      ans.begin();
-
-            // We update the element at the
-            // found position with the current number.
-            // By doing this, we are maintaining
-            // a sorted order in the answer vector.
-            ans[low] = v[i];
+            // If arr[i] is not greater, replace the element in temp with arr[i]
+            int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            temp[ind] = arr[i];
         }
     }
 
-    // The length of the answer vector
-    // represents the length of the
-    // longest increasing subsequence.
-    return ans.size();
+    return len;
 }
 
 int main() {
@@ -52,10 +27,12 @@ int main() {
     cin >> n;
     vector<int> v(n);
     for (int i = 0; i < n; i++) cin >> v[i];
-    printf("Length of LIS is %d\n", lengthOfLIS(v));
-    for (auto it : ans) {
+
+    cout << longestIncreasingSubsequence(v, n) << endl;
+    for (auto it : temp) {
         cout << it << " ";
     }
     cout << endl;
+
     return 0;
 }
---
+++ LIS/LIS_UsingBinarySearchO(nLogn)TimeandO(n)SpacewithPrintSequence.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define inputarr(v, n) \
+    for (int i = 0; i < n; i++) cin >> v[i];
+
+#define printarr(v)                    \
+    for (int i = 0; i < v.size(); i++) \
+        cout << v[i] << " ";           \
+    cout << endl;
+const int N = 100005;
+//---------------------------------------//
+
+void lis(vector<int>& v) {
+    int n = v.size();
+    vector<int> ans, ansin;
+    vector<int> prev(n, -1);
+    for (int i = 0; i < n; i++) {
+        int pos = lower_bound(ans.begin(), ans.end(), v[i]) - ans.begin();
+        if (pos < ans.size()) {
+            ans[pos] = v[i];
+            ansin[pos] = i;
+        } else {
+            ans.push_back(v[i]);
+            ansin.push_back(i);
+        }
+        if (pos > 0) {
+            prev[i] = ansin[pos - 1];
+        }
+    }
+    int i = ansin.back();
+    vector<int> res;
+    while (i != -1) {
+        res.push_back(i);
+        i = prev[i];
+    }
+
+    cout << ans.size() << endl;
+    for (int i = res.size() - 1; i >= 0; i--) {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    lis(v);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1, Case = 1;
+    // cin>>t;
+    while (t--) {
+        // cout << "Case " << Case++ << ": ";
+        solve();
+    }
+    return 0;
+}
